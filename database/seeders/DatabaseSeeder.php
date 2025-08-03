3a8879b0d798431ca22b6644e8f47b2c7b8ca151
mkbcentral <mkbcentralcode@gmail.com>
2025-08-03 17:55:04 +0200

    Initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        /*
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
         CategoryTraining::insert([
             ['name' => 'Crypto'],
             ['name' => 'Data Science'],
             ['name' => 'Cybersécurité'],
             ['name' => 'Autres'],
         ]);
-        */
+
         CategoryPost::insert([
             ['name' => 'Crypto'],
             ['name' => 'Data Science'],
